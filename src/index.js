44f3c78b298ee5b256e5369965ba850fc73b9b7e
John John Penafiel <johnjohnpenafiel@hotmail.com>
2024-04-25 15:56:47 -0400

    Task List Challenge

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+
+// adding to the list
+document.querySelector('#create-task-form').addEventListener('submit', (event) => {
+event.preventDefault();
+const task = event.target['new-task-description'].value;
+const listing = document.createElement('li');
+listing.textContent = task;
+document.querySelector("#tasks").append(listing);
+document.querySelector('form').reset();
+})
 });
+
+
+
