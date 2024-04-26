5bb86a35f56d583c40a73d40c05625c253a470e6
John John Penafiel <johnjohnpenafiel@hotmail.com>
2024-04-26 10:46:08 -0400

    added Delete Button

--- src/index.js
+++ src/index.js
@@ -7,9 +7,22 @@ const task = event.target['new-task-description'].value;
 const listing = document.createElement('li');
 listing.textContent = task;
 document.querySelector("#tasks").append(listing);
+
+// delete from the list
+
+    const deleteButton = document.createElement('button');
+    deleteButton.textContent = 'X';
+    deleteButton.addEventListener('click', () => {
+        listing.remove()
+    })
+listing.append(deleteButton);
+document.querySelector('#tasks').append(listing);
 document.querySelector('form').reset();
 })
+
+
 });
+
 
 
 
